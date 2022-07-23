ada052a0d16d23ee647c48ef44ff18f8637d0c7d
huzidev <huzaifa.iqdev@gmail.com>
2022-07-23 12:13:28 +0500

    props for function

--- typescript-react/src/components/App.tsx
+++ typescript-react/src/components/App.tsx
@@ -5,14 +5,18 @@ import InputField from './InputField';
 export default function App(): JSX.Element {
 
     const [todo, setTodo] = useState<string>(""); // means initial state is Empty and type is string
-    const [todos, setTodos] = useState<Todo[]>([]); // initial state is empty array and type is of array of whoms interface we've created in separate components called model.ts
+    const [todos, setTodos] = useState<Todo[]>([]); // initial state is empty array and type is of array of todos whoms interface we've created in separate components called model.ts
+
+    function handleAdd () {
+
+    }
 
     return (
         <div>
             <h1>
                 test
             </h1>
-            <InputField todo={todo} setTodo={setTodo}/>
+            <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
         </div>
     )
 }
--- typescript-react/src/components/InputField.tsx
+++ typescript-react/src/components/InputField.tsx
@@ -4,12 +4,13 @@ import React from 'react'
 interface PropsTypes {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>; // just hover on setTodo part in react useState
+    handleAdd: () => void; // for function types
 }
 
-export default function InputField({todo, setTodo}: PropsTypes): JSX.Element {
+export default function InputField({todo, setTodo, handleAdd}: PropsTypes): JSX.Element {
     return (
         <div>
-            <form >
+            <form onSubmit={handleAdd}>
                 <input 
                     type="text"
                     value={todo}

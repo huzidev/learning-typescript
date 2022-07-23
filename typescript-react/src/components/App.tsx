cba6ecd87181a3ac6f240dfe17c8564b7b2cf368
huzidev <huzaifa.iqdev@gmail.com>
2022-07-23 12:08:56 +0500

    export interface

--- typescript-react/src/components/App.tsx
+++ typescript-react/src/components/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { Todo } from './Model';
 import InputField from './InputField';
 
 export default function App(): JSX.Element {
 
-    const [todo, setTodo] = useState<string>(""); // means initial state is Empty String
+    const [todo, setTodo] = useState<string>(""); // means initial state is Empty and type is string
+    const [todos, setTodos] = useState<Todo[]>([]); // initial state is empty array and type is of array of whoms interface we've created in separate components called model.ts
 
     return (
         <div>

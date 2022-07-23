75293baea3314e61278201e9753dbc499bc3e320
huzidev <huzaifa.iqdev@gmail.com>
2022-07-23 11:53:14 +0500

    types added

--- typescript-react/src/components/InputField.tsx
+++ typescript-react/src/components/InputField.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 // for all the types of props we are receiving
 interface Props {
-
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>; // just hover on setTodo part in react useState
 }
 
-export default function InputField({}) {
+export default function InputField({todo, setTodo}: Props): JSX.Element {
     return (
         <div>
             <form >

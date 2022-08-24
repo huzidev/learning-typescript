4f0f4cd9026d2a3d561baa93e812219d31fc33ff
huzidev <huzaifa.iqdev@gmail.com>
2022-08-24 20:48:46 +0500

    buttons added

--- project/happyhomes-web/src/pages/ApartmentView/index.tsx
+++ project/happyhomes-web/src/pages/ApartmentView/index.tsx
@@ -70,14 +70,19 @@ function ViewApartmentPage(): JSX.Element {
                 title="403"
                 subTitle={`The apartment you tries to access might be Rented would you like to check more apartment from the user who've uploaded that apartment`}
                 extra={
-                  <Button
-                    type="primary"
-                    onClick={() => 
-                      history.push(`${ROUTE_PATHS.VIEW_PROFILE}${state.data?.realtorId}`)
-                    }
-                  >
-                    Check More
-                  </Button>
+                  <>
+                    <Button
+                      type="primary"
+                      onClick={() =>
+                        history.push(`${ROUTE_PATHS.VIEW_PROFILE}${state.data?.realtorId}`)
+                      }
+                    >
+                      Check More
+                    </Button>
+                    <Button type="primary" onClick={() => history.replace(ROUTE_PATHS.DASHBOARD)}>
+                      Back Home
+                    </Button>
+                  </>
                 }
               />
             </Card>

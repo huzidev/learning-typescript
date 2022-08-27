e5103a36355166e0369107d463fb27e40e18b183
huzidev <huzaifa.iqdev@gmail.com>
2022-08-27 17:13:07 +0500

    interfaces for function

--- todo-zustand/src/components/store/types.ts
+++ todo-zustand/src/components/store/types.ts
@@ -1,4 +1,13 @@
 export interface Store {
     // array[] of Todo list
     todos: Todo[];
+
+    // newTodo of type string for typing new todo
+    newTodo: string;
+
+    // addTodo function with no-parameter
+    addTodo: () => void;
+
+    // this will update the value of newTodo therefore parameter is (text of type string)
+    setNewTodo: (text: string) => void;
 }

23d9bddccb0579255c28b883544da77024d7bda7
huzidev <huzaifa.iqdev@gmail.com>
2022-08-27 21:19:37 +0500

    todos list fix

--- todo-zustand/src/components/App.tsx
+++ todo-zustand/src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Row } from 'antd';
 import AddTodo from './todo/AddTodo';
+import TodoList from './todo/TodoList';
 import Top from './todo/Top';
 import 'antd/dist/antd.css';
 
@@ -10,6 +11,7 @@ export default function App() {
       <Col span={20}>
         <Top />
         <AddTodo />
+        <TodoList />
       </Col>
     </div>
   )

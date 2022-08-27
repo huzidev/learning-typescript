e716c09f6e4fa59ca938655eca971b6b0fb8131f
huzidev <huzaifa.iqdev@gmail.com>
2022-08-27 21:10:53 +0500

    todo item

--- todo-zustand/src/components/todo/TodoList.tsx
+++ todo-zustand/src/components/todo/TodoList.tsx
@@ -1,10 +1,16 @@
-import React from 'react;
+import React from 'react';
 import useStore from '../store'
+import { Input, Button, Col, Row } from 'antd';
 
 export default function TodoList() {
+  const store = useStore();
   return (
     <div>
-
+      {store.todos.map((data) => (
+        <div key={1}>
+          <Input value={todo.text} />
+        </div>
+      ))}
     </div>
   )
 }

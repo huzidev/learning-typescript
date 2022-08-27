f1954304bfe3e66daeeafe2a7e52e5d739dd9e47
huzidev <huzaifa.iqdev@gmail.com>
2022-08-27 21:56:26 +0500

    toggle function

--- todo-zustand/src/components/todo/TodoList.tsx
+++ todo-zustand/src/components/todo/TodoList.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import useStore from '../store'
-import { Input, Button, Col, Row } from 'antd';
+import { Input, Button, Col, Row, Checkbox } from 'antd';
+import type { CheckboxChangeEvent } from 'antd/es/checkbox';
 
 export default function TodoList() {
   const store = useStore();
+  const onChange = (e: CheckboxChangeEvent) => {
+    console.log(`checked = ${e.target.checked}`);
+  };
   return (
     <div>
       {store.todos.map((data) => (
         <div key={data.id}>
           <Input value={data.text} />
+          <Checkbox 
+            onChange={onChange}
+            checked={data.done}
+            onClick={() => store.toggle(data.id)}
+          >
+              Checkbox
+          </Checkbox>
         </div>
       ))}
     </div>

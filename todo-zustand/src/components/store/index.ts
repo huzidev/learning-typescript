8a0c62ce89adfb1a51f1e8e5ab731efe7fbd2f5e
huzidev <huzaifa.iqdev@gmail.com>
2022-08-27 17:16:29 +0500

    create hook zustand

--- todo-zustand/src/components/store/index.ts
+++ todo-zustand/src/components/store/index.ts
@@ -2,3 +2,8 @@ import create from 'zustand';
 
 // all the interfaces are defined in the types file
 import { Store } from './types';
+
+// here we've used ours interface Store in which types are defined like todos: Todo[], newTodo: string, addTodo function
+const useStore = create<Store>((set) => ({
+
+}))

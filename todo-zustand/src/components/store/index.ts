222506a756e9f905aaeec6d729203813c986175c
huzidev <huzaifa.iqdev@gmail.com>
2022-08-27 17:35:48 +0500

    parameters

--- todo-zustand/src/components/store/index.ts
+++ todo-zustand/src/components/store/index.ts
@@ -5,5 +5,14 @@ import { Store } from './types';
 
 // here we've used ours interface Store in which types are defined like todos: Todo[], newTodo: string, addTodo function
 const useStore = create<Store>((set) => ({
-
+    todos: [],
+    newTodo: "",
+    addTodo() {
+        set((state) => ({
+            ...state,
+            // since we are adding newTodo through addTodo function therefore todos: addTodo(state.todos, state.newTodo)
+            // in addTodo we've passed 2 parameter state.todos is the initial state and state.newTodo is the new todo text that is going to be added in todos: Todo[] (array)
+            todos: this.addTodo(state.todos, state.newTodo)
+        }))
+    }
 }))

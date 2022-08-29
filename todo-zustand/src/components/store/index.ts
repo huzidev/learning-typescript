ecbcde3757f699acf2f57ce550bae6ba033a618f
huzidev <huzaifa.iqdev@gmail.com>
2022-08-29 10:34:31 +0500

    componets works

--- todo-zustand/src/components/store/index.ts
+++ todo-zustand/src/components/store/index.ts
@@ -1,45 +1,7 @@
 import create from 'zustand';
-
 // all the interfaces are defined in the types file
 import { Store } from './types';
 import * as actions from './actions';
-// import { Todo } from './types';
-
-
-
-// // for updating todo
-// // here todos: Todo[] will takes all the data of todos in an array means it is complete list of todos therefore we've used (todos.map) function because todos will have all the data
-// const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
-//   todos.map((data) => ({
-//     ...data,
-//     text: data.id === id ? text : data.text,
-//   }));
-
-
-// // for check-box
-// const toggleTodo = (todos: Todo[], id: number): Todo[] =>
-//   todos.map((data) => ({
-//     ...data,
-//     done: data.id === id ? !data.done : data.done,
-//   }));
-
-
-// // for deleting todo
-// const removeTodo = (todos: Todo[], id: number): Todo[] =>
-//   todos.filter((data) => data.id !== id);
-
-
-// // for adding todo
-// const addTodo = (todos: Todo[], text:string): Todo[] => [
-//     ...todos,
-//     {
-//         id: Math.max(0, Math.max(...todos.map(({id}) => id))) + 1,
-//         text,
-//         // by default done (check-box) will be false
-//         done: false
-//     },
-// ];
-
 
 // here we've used ours interface Store in which types are defined like todos: Todo[], newTodo: string, addTodo function
 export const useStore = create<Store>((set) => ({

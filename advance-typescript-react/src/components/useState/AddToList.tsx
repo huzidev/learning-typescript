dd652ebc28f09f8a44b7ece53ed7b8283ed632e6
huzidev <huzaifa.iqdev@gmail.com>
2022-08-30 21:48:25 +0500

    state management

--- advance-typescript-react/src/components/useState/AddToList.tsx
+++ advance-typescript-react/src/components/useState/AddToList.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AddToList() {
+
+  const [input, setInput] = useState({
+    name: "",
+    age: "",
+    note: ""
+  })
+
   return (
     <div>
         Add To list
         <input 
             type="text"
             placeholder="Name"
-        />
+            name="name"
+            value={input.name}
+            />
         <input 
             type="text"
             placeholder="Age"
-        />
+            name="age"
+            value={input.age}
+            />
         <input 
             type="text"
             placeholder="Note (Optional)"
+            name="note"
+            value={input.note}
         />
     </div>
   )

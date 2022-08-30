4020391a470da381b6494e7743685c0130a69dfd
huzidev <huzaifa.iqdev@gmail.com>
2022-08-30 21:44:23 +0500

    input tags

--- advance-typescript-react/src/components/useState/AddToList.tsx
+++ advance-typescript-react/src/components/useState/AddToList.tsx
@@ -4,6 +4,18 @@ export default function AddToList() {
   return (
     <div>
         Add To list
+        <input 
+            type="text"
+            placeholder="Name"
+        />
+        <input 
+            type="text"
+            placeholder="Age"
+        />
+        <input 
+            type="text"
+            placeholder="Note (Optional)"
+        />
     </div>
   )
 }

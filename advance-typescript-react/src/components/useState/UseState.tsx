9353abbd0edc299ec668ee204d9e26e5ee592d95
huzidev <huzaifa.iqdev@gmail.com>
2022-08-30 21:29:14 +0500

    interface

--- advance-typescript-react/src/components/useState/UseState.tsx
+++ advance-typescript-react/src/components/useState/UseState.tsx
@@ -6,22 +6,31 @@ export default function UseState() {
   // defining types with useState
   const [number, setNumber] = useState<number>(0);
   const [state, setState] = useState<boolean>(false);
-  const [input, setInput] = useState<string>('');
-
+  
   // for increment number by one
   function inc() {
     setNumber((prevState) => prevState + 1);
   }
-
+  
   function chnageState() {
     setState((prevState) => !prevState)
   }
-
+  
   const showState = state ? 'True' : 'False'
+  
 
-  function handleInput(event: React.FormEvent) {
-    event.preventDefault()
+
+  // for array of objects {}[]
+
+  interface Data {
+    name: string,
+    age: number,
+    // means note of type optional
+    note?: string
   }
+
+  // here if we din't used (DATA) which is ours interface then we cab't specify that whether is is array of number, array of strings, array of boolean IF we just used useState([])
+  const [person, setPerson] = useState<Data>([])
 
   return (
     <div>

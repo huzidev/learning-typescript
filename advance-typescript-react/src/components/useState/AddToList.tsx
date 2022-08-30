01da6722c50c1027e2febb40d94224dd240ad3f0
huzidev <huzaifa.iqdev@gmail.com>
2022-08-30 22:06:34 +0500

    event type for handleChange

--- advance-typescript-react/src/components/useState/AddToList.tsx
+++ advance-typescript-react/src/components/useState/AddToList.tsx
@@ -8,10 +8,14 @@ export default function AddToList() {
     note: ""
   });
 
-  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+
+  // now how do we get to know what can be the type for (event) here it's simple
+  // we know that we are creating (handleInput) function for (onChange) in input tag so if we creates a function inside onChange like 
+  // onChange={(event) => } then we'll not get any error and if we hover over the event here we can see the type which is (React.ChangeEvent<HTMLInputElement>) then simple past the type here
+  function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
     setInput({
         ...input,
-        [e.target.name]: e.target.value
+        [event.target.name]: event.target.value
     })  
   }
 

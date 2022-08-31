df16a5790edee9f84d7614d85a14efeb5397fa9d
huzidev <huzaifa.iqdev@gmail.com>
2022-08-31 12:36:12 +0500

    home page

--- advance-typescript-react/src/components/App.tsx
+++ advance-typescript-react/src/components/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import UseState from './useState/UseState';
 
 export default function App() {
   return (
     <div>
-        <UseState />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<UseState />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ advance-typescript-react/src/components/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import UseState from './useState/UseState';
+
+export default function Home() {
+  return (
+    <div>
+        <h3>
+            Home page
+        </h3>
+        <ul>
+            <li>
+
+            </li>
+        </ul>
+    </div>
+  )
+}

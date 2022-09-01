06795802dbbf3e661dbff4b535d2593b19cf855f
huzidev <huzaifa.iqdev@gmail.com>
2022-09-01 09:27:55 +0500

    props

--- advance-typescript-react/src/components/cart/pages/Store.tsx
+++ advance-typescript-react/src/components/cart/pages/Store.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Items from '../../data/items.json';
+import { Storeitems } from '../../storeitems/Storeitems';
+import { Container } from '../../styled-components/Container.styled';
 
 export function Store() {
   return (
-    <div>
+    <Container>
       Store
-    </div>
+      <div>
+        {Items.map((data) => (
+          <div key={data.id}>
+            {/* passing props */}
+            <Storeitems {...data} />
+          </div>
+        ))
+        }
+      </div>
+    </Container>
   )
 }
 

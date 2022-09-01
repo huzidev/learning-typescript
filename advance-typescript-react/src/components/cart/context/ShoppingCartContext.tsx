64ec9ffbb9ed714e601ab70f7ae41afe1897f7ce
huzidev <huzaifa.iqdev@gmail.com>
2022-09-01 10:42:39 +0500

    context hook

---
+++ advance-typescript-react/src/components/cart/context/ShoppingCartContext.tsx
@@ -0,0 +1,21 @@
+import { createContext, ReactNode, useContext } from "react";
+
+interface ShoppingCartProviderProps {
+    // ReactNode is type for children property
+    children: ReactNode
+}
+
+const ShoppingCartContext = createContext({})
+
+// custom hook
+export function useShoppingCart() {
+    return useContext(ShoppingCartContext)
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+    return(
+        <ShoppingCartContext.Provider value={{}}>
+            {children}
+        </ShoppingCartContext.Provider>
+    )
+}

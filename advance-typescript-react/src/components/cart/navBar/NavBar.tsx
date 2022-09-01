16f493b251e88ce069874cc86a794de1bd0a3e45
huzidev <huzaifa.iqdev@gmail.com>
2022-09-01 09:13:50 +0500

    nav link added

--- advance-typescript-react/src/components/cart/navBar/NavBar.tsx
+++ advance-typescript-react/src/components/cart/navBar/NavBar.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Nav } from '../../styled-components/Nav.styled';
 import { Container } from '../../styled-components/Container.styled';
+import { Link } from 'react-router-dom';
 
 export function NavBar() {
   return (
       <Nav>
-        NavBar
+        <Link to='/cart/data' className='link'>
+          Data
+        </Link>
+        <Link to='/cart/about' className='link'>
+          About
+        </Link>
+        <Link to='/cart/store'className='link'>
+          Store
+        </Link>
       </Nav>
   )
 }
--- advance-typescript-react/src/components/styled-components/Nav.styled.ts
+++ advance-typescript-react/src/components/styled-components/Nav.styled.ts
@@ -4,4 +4,7 @@ import styled from "styled-components";
 export const Nav = styled.div`
     background-color: aqua;
     padding: 20px;
+    .link {
+        margin: 10px;
+    }
 `

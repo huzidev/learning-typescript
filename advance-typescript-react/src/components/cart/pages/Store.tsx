96e11246f6a70c4a1676166180b3c58bc4d03360
huzidev <huzaifa.iqdev@gmail.com>
2022-09-01 09:22:35 +0500

    json data imported

--- advance-typescript-react/src/components/cart/pages/Store.tsx
+++ advance-typescript-react/src/components/cart/pages/Store.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Items from '../../data/items.json';
 
 export function Store() {
   return (
-    <div>Store</div>
+    <div>
+      Store
+    </div>
   )
 }
 

3e5b533d242c8a1ced3bd2991df6c3d751cc046d
huzidev <huzaifa.iqdev@gmail.com>
2022-09-01 11:39:30 +0500

    functions destructure

--- advance-typescript-react/src/components/cart/storeitems/Storeitems.tsx
+++ advance-typescript-react/src/components/cart/storeitems/Storeitems.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { FormatCurrency } from '../utils/FormatCurrency';
 
 import { ItemsType } from '../types/Types'
+import { useShoppingCart } from '../context/ShoppingCartContext';
 
 export function Storeitems({ id, name, price, url }: ItemsType): JSX.Element {
+
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart
+  } = useShoppingCart()
+
+  const quantity = getItemQuantity(id)
+
   return (
     <>
       <img src={url} alt="product-img" />

f3f1343557e57e32f095286459214239ad6e17dd
huzidev <huzaifa.iqdev@gmail.com>
2022-09-02 21:16:07 +0500

    styled components added

--- typescript-adonisJS/src/components/App.tsx
+++ typescript-adonisJS/src/components/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Error from './error/Error';
+import { GlobalStyles } from './styled-components/Global.styled';
 
-export default function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+export default function App(): JSX.Element {
   return (
     <div>
-        App
+      <Router>
+        <GlobalStyles />
+        <Routes>
+            <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </div>
   )
 }

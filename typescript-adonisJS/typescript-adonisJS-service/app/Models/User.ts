2d6ec2e8571c742aea4ddbfcd772d3fddb8d8e7a
huzidev <huzaifa.iqdev@gmail.com>
2022-09-03 17:07:26 +0500

    base model for database

--- typescript-adonisJS/typescript-adonisJS-service/app/Models/User.ts
+++ typescript-adonisJS/typescript-adonisJS-service/app/Models/User.ts
@@ -1,1 +1,22 @@
 import { DateTime } from 'luxon'
+import pick from 'lodash/pick'
+import Hash from '@ioc:Adonis/Core/Hash'
+import {
+  column,
+  beforeSave,
+  BaseModel,
+  afterSave,
+  afterFetch,
+  afterFind,
+  scope,
+  hasMany,
+  HasMany,
+} from '@ioc:Adonis/Lucid/Orm'
+
+export type UserRole = typeof User.roles[number]
+
+// BaseModel for interaction with Database
+export default class User extends BaseModel {
+
+}
+

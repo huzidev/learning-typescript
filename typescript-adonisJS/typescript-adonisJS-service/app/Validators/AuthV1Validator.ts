9532643dc927e905d8bd6af38a97d2a52a36990d
huzidev <huzaifa.iqdev@gmail.com>
2022-09-03 15:24:43 +0500

    schemea

--- typescript-adonisJS/typescript-adonisJS-service/app/Validators/AuthV1Validator.ts
+++ typescript-adonisJS/typescript-adonisJS-service/app/Validators/AuthV1Validator.ts
@@ -1,8 +1,14 @@
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
+// SCHEMA for SignIn, SignUp, ResetPass etc
+
 export const authValidationMessages = {
   'required': '{{ field }} is required to sign up',
   'name.alpha': 'Invalid name',
   'email.email': 'Invalid email address',
   'password.minLength': 'Password should be at least 6 characters long',
+}
+
+export class AuthV1SignIn {
+
 }

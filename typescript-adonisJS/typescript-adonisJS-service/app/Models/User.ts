4f63fcd7c2d4ec19821dc442a99c0b67e2aa0d74
huzidev <huzaifa.iqdev@gmail.com>
2022-09-03 17:42:00 +0500

    before save hook adonis

--- typescript-adonisJS/typescript-adonisJS-service/app/Models/User.ts
+++ typescript-adonisJS/typescript-adonisJS-service/app/Models/User.ts
@@ -21,5 +21,49 @@ export default class User extends BaseModel {
 
   // getRoleIndex for loggedIn user's role
   public static getRoleIndex = (role: UserRole): any => User.roles.indexOf(role)
-}
 
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public rememberMeToken?: string
+
+  @column()
+  public role: UserRole
+
+  // boolean values
+  @column({ consume: (value) => !!value })
+  public isActive: boolean = true
+
+  @column({ consume: (value) => !!value })
+  public isBanned: boolean = false
+
+  @column({ consume: (value) => !!value })
+  public isVerified: boolean = false
+
+  @column({ consume: (value) => !!value })
+  public isTheme: boolean = false
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  // beforeSave is called every time a new user is created or updated
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+}

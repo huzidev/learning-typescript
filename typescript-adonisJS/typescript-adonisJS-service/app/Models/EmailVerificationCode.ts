425c7fc25de82eb77d5b3101494bdda8b8490ed4
huzidev <huzaifa.iqdev@gmail.com>
2022-09-05 13:44:39 +0500

    type if

--- typescript-adonisJS/typescript-adonisJS-service/app/Models/EmailVerificationCode.ts
+++ typescript-adonisJS/typescript-adonisJS-service/app/Models/EmailVerificationCode.ts
@@ -1,9 +1,21 @@
-import { DateTime } from 'luxon'
 import { BaseModel, beforeCreate, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+import random from 'lodash/random'
+
+import User from './User'
 
 export default class EmailVerificationCode extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public code: string
+
+  @column()
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime

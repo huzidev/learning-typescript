ad1405e143d3f7f2847cc745cdbc5672468a2de3
huzidev <huzaifa.iqdev@gmail.com>
2022-09-05 10:05:48 +0500

    schema for migrations

---
+++ typescript-adonisJS/typescript-adonisJS-service/database/migrations/1662353919906_users.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class UsersSchema extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('name', 100).unique().notNullable().index()
+      table.string('email', 255).unique().notNullable().index()
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}

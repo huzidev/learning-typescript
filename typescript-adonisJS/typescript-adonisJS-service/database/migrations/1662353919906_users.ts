770fa492f343aad3cd8185daf311cd4cdd620a66
huzidev <huzaifa.iqdev@gmail.com>
2022-09-05 10:11:42 +0500

    migration schema

--- typescript-adonisJS/typescript-adonisJS-service/database/migrations/1662353919906_users.ts
+++ typescript-adonisJS/typescript-adonisJS-service/database/migrations/1662353919906_users.ts
@@ -8,6 +8,13 @@ export default class UsersSchema extends BaseSchema {
       table.increments('id').primary()
       table.string('name', 100).unique().notNullable().index()
       table.string('email', 255).unique().notNullable().index()
+      table.string('password', 180).notNullable()
+      table.string('remember_me_token').nullable()
+
+      table.boolean('is_active').defaultTo(true).notNullable()
+      table.boolean('is_banned').defaultTo(false).notNullable()
+      table.boolean('is_verified').defaultTo(false).notNullable()
+      table.boolean('is_theme').defaultTo(false).notNullable()
 
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })

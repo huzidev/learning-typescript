2384e5464111e3a442e3b40f7e5f51a730f580be
huzidev <huzaifa.iqdev@gmail.com>
2022-09-05 13:57:52 +0500

    fucntions for signIn

--- typescript-adonisJS/typescript-adonisJS-service/app/Controllers/AuthV1Controller.ts
+++ typescript-adonisJS/typescript-adonisJS-service/app/Controllers/AuthV1Controller.ts
@@ -3,3 +3,11 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { AuthV1SignIn } from 'App/Validators/AuthV1Validator'
 import User from 'App/Models/User'
 import Database from '@ioc:Adonis/Lucid/Database'
+import EmailVerificationCode from 'App/Models/EmailVerificationCode'
+import { DateTime } from 'luxon'
+
+export default class AuthV1Controller {
+  public async signIn({ request, auth }: HttpContextContract) {
+
+  }
+}

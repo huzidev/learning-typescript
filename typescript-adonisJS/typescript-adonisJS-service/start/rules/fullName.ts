e9e6c440d3707fca27097a7c4c3d8f1223ca95da
huzidev <huzaifa.iqdev@gmail.com>
2022-09-05 09:50:35 +0500

    rule for name

---
+++ typescript-adonisJS/typescript-adonisJS-service/start/rules/fullName.ts
@@ -0,0 +1,28 @@
+import { validator } from '@ioc:Adonis/Core/Validator'
+
+validator.rule(
+  'fullName',
+  // pointer, arrayExpressionPointer, errorReporter are options, necessary to use
+  async (value, _, { pointer, arrayExpressionPointer, errorReporter }) => {
+    const regex = /^[a-zA-Z ]*$/
+
+    if (!regex.test(value)) {
+      // .report() receives 4 parameters
+      errorReporter.report(pointer, 'match', 'only alphabets are allowed', arrayExpressionPointer)
+      return
+    }
+
+    const trimmed = value.trim()
+    if (trimmed.length < 2) {
+      errorReporter.report(
+        pointer,
+        'minLength',
+        `${pointer} must be 2 char long`,
+        arrayExpressionPointer
+      )
+    }
+  },
+  () => ({
+    async: true,
+  })
+)

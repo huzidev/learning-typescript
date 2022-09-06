8e7fcabb0a46dcf939424883b75aa6531c04c07a
huzidev <huzaifa.iqdev@gmail.com>
2022-09-06 21:34:40 +0500

    user roles

--- typescript-adonisJS/typescript-adonisJS-web/src/store/auth/types.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/store/auth/types.ts
@@ -1,1 +1,26 @@
 import { HTTPFieldErrors, SubState } from '@store/types';
+
+export interface InitSubType extends SubState {
+    init: boolean;
+}
+
+
+export const roles = ['client', 'realtor', 'admin', 'super-admin'] as const;
+export type UserRole = typeof roles[number];
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    role: UserRole;
+    isActive: boolean;
+    isTheme: boolean;
+    isVerified: boolean;
+    isBanned: boolean;
+}
+
+export interface AuthState {
+    signInState: SubState
+    signUpState: SubState
+    signOutState: SubState
+}

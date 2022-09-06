0b3969906efefdd0b197a82d2d5e2bc3662ac7be
huzidev <huzaifa.iqdev@gmail.com>
2022-09-06 10:31:55 +0500

    paths added for login Page

--- typescript-adonisJS/typescript-adonisJS-service/start/routes/auth/v1.ts
+++ typescript-adonisJS/typescript-adonisJS-service/start/routes/auth/v1.ts
@@ -8,5 +8,17 @@ export default function AuthV1Routes() {
     Route.post('/sing_in', `${path}.signIn`)
     Route.post('/sign_up', `${path}.signUp`)
     Route.post('/sign_out', `${path}.signOut`).middleware('auth:any')
-  })
+
+    // Verification Email
+    Route.post('verify_email/send_code', `${path}.sendCodeForVerifyEmail`).middleware(
+      'auth:no_verify'
+    )
+    Route.post('verify_email/verify_code', `${path}.verifyCodeForEmail`).middleware(
+      'auth:no_verify'
+    )
+
+    // Reset Password
+    Route.post('reset_password/send_code', `${path}.sendCodeForResetPassword`)
+    Route.post('reset_password', `${path}.resetPassword`)
+  }).prefix('v1')
 }

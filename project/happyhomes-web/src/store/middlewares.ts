fd7def729f61a1d7e753db7554b0f95728db70d1
huzidev <huzaifa.iqdev@gmail.com>
2022-09-06 19:46:07 +0500

    generic ts

--- project/happyhomes-web/src/store/middlewares.ts
+++ project/happyhomes-web/src/store/middlewares.ts
@@ -1,6 +1,7 @@
 import { State, StateCreator } from 'zustand';
 import produce, { Draft } from 'immer';
 
+// <T> is generic and is used when type is not same means it can be string, object, int therefore we can just use <T> generic
 export const immerMiddleware =
   <T extends State>(config: StateCreator<T>): StateCreator<T> =>
   (set, get, api) =>

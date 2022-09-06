9a3b8d22717b93cb3698c1b19b0a51816225668d
huzidev <huzaifa.iqdev@gmail.com>
2022-09-06 12:20:12 +0500

    adding paths

--- typescript-adonisJS/typescript-adonisJS-web/src/Router/paths.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/Router/paths.ts
@@ -1,3 +1,8 @@
 const ROUTE_PATH = {
-    
+    HOME: '/',
+    DASHBOARD: '/home',
+    VERIFY_USER: '/verify_user',
+    BANNED_USER: '/banned_user',
+    RESET_PASSWORD: '/reset_password',
+    SEND_RESET_PASSWORD_CODE: '/send_reset_password_code',
 }

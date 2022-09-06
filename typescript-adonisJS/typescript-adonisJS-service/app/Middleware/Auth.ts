67134ed2bf09a88f5ef24ece8830b0325b03a9e9
huzidev <huzaifa.iqdev@gmail.com>
2022-09-06 10:09:50 +0500

    middleware

---
+++ typescript-adonisJS/typescript-adonisJS-service/app/Middleware/Auth.ts
@@ -0,0 +1,75 @@
+import { GuardsList } from '@ioc:Adonis/Addons/Auth'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { AuthenticationException } from '@adonisjs/auth/build/standalone'
+import User, { UserRole } from 'App/Models/User'
+
+/**
+ * Auth middleware is meant to restrict un-authenticated access to a given route
+ * or a group of routes.
+ *
+ * You must register this middleware inside `start/kernel.ts` file under the list
+ * of named middleware.
+ */
+export default class AuthMiddleware {
+  /**
+   * The URL to redirect to when request is Unauthorized
+   */
+  protected redirectTo = '/login'
+
+  /**
+   * Authenticates the current HTTP request against a custom set of defined
+   * guards.
+   *
+   * The authentication loop stops as soon as the user is authenticated using any
+   * of the mentioned guards and that guard will be used by the rest of the code
+   * during the current request.
+   */
+  protected async authenticate(auth: HttpContextContract['auth'], guards: (keyof GuardsList)[]) {
+    /**
+     * Hold reference to the guard last attempted within the for loop. We pass
+     * the reference of the guard to the "AuthenticationException", so that
+     * it can decide the correct response behavior based upon the guard
+     * driver
+     */
+    let guardLastAttempted: string | undefined
+
+    for (let guard of guards) {
+      guardLastAttempted = guard
+
+      if (await auth.use(guard).check()) {
+        /**
+         * Instruct auth to use the given guard as the default guard for
+         * the rest of the request, since the user authenticated
+         * succeeded here
+         */
+        auth.defaultGuard = guard
+        return true
+      }
+    }
+
+    /**
+     * Unable to authenticate using any guard
+     */
+    throw new AuthenticationException( // this error will be shown in notification if user's token get expired and user try to access something then this error will be shown
+      'Unauthorized access',
+      'E_UNAUTHORIZED_ACCESS',
+      guardLastAttempted,
+      this.redirectTo
+    )
+  }
+
+  /**
+   * Handle request
+   */
+  // add [role] and UserRole[]
+  public async handle(
+    { auth }: HttpContextContract,
+    next: () => Promise<void>,
+    [role]: string[] | UserRole[]
+  ) {
+    await this.authenticate(auth, [auth.name])
+
+
+    await next()
+  }
+}

6ba8467fc807c69b873130015ab821f88abdf285
huzidev <huzaifa.iqdev@gmail.com>
2022-09-06 09:25:41 +0500

    login page route

--- typescript-adonisJS/typescript-adonisJS-service/start/routes/auth/v1.ts
+++ typescript-adonisJS/typescript-adonisJS-service/start/routes/auth/v1.ts
@@ -1,7 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
+import AuthV1Controller from 'App/Controllers/AuthV1Controller'
 
 export default function AuthV1Routes() {
+  const path = AuthV1Controller
+
   Route.group(() => {
-    Route.post('/signin')
+    Route.post('/sing_in', `${path}.signIn`)
+    Route.post('/sign_up', `${path}.signUp`)
   })
 }

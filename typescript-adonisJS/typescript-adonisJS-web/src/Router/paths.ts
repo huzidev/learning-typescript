1acf21db696389c0f72272cc7053d4a4f7541ae2
huzidev <huzaifa.iqdev@gmail.com>
2022-09-06 12:20:57 +0500

    users paths

--- typescript-adonisJS/typescript-adonisJS-web/src/Router/paths.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/Router/paths.ts
@@ -5,4 +5,10 @@ const ROUTE_PATH = {
     BANNED_USER: '/banned_user',
     RESET_PASSWORD: '/reset_password',
     SEND_RESET_PASSWORD_CODE: '/send_reset_password_code',
+
+    VIEW_PROFILE: '/user/view/',
+    USERS: '/user/list/',
+    USER_CREATE: '/user/create',
+    USER_UPDATE: '/user/update/',
+    USER_SETTING: '/user/settings/',
 }

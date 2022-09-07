c4346acabf928116ac3fdae83ee96e1dcea299df
huzidev <huzaifa.iqdev@gmail.com>
2022-09-07 10:00:29 +0500

    subStates zustand

--- project/happyhomes-web/src/store/auth/index.ts
+++ project/happyhomes-web/src/store/auth/index.ts
@@ -7,6 +7,8 @@ import * as actions from './actions';
 
 export const useAuth = create<AuthState>(
   immerMiddleware((set) => ({
+    // subState with camel case are the initial Values ex loading: false and error: false
+    // SubState with Capital (S) is the interface means types
     signInState: subState,
     signUpState: subState,
     initState: { ...subState, init: false },

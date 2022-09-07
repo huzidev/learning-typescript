e337ee7e659b77ed1c383702cb623810b49c606c
huzidev <huzaifa.iqdev@gmail.com>
2022-09-07 10:39:01 +0500

    default values

--- typescript-adonisJS/typescript-adonisJS-web/src/store/auth/index.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/store/auth/index.ts
@@ -5,3 +5,12 @@ import { immerMiddleware } from '@store/middlewares';
 // initial VALUES
 import { subState } from '@store/state';
 import { AuthState } from './types';
+
+
+export const useAuth = create<AuthState>(
+    immerMiddleware(set => ({
+        signInState: subState
+        signUpState: subState
+        signOutState: subState
+    }))
+)

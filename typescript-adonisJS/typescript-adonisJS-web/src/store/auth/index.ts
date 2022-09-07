1fb3b8713d2019e943f7fe0b5adcee73da1b8cd7
huzidev <huzaifa.iqdev@gmail.com>
2022-09-07 10:01:47 +0500

    zustand actions

--- typescript-adonisJS/typescript-adonisJS-web/src/store/auth/index.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/store/auth/index.ts
@@ -1,1 +1,7 @@
 import create from 'zustand';
+
+import { immerMiddleware } from '@store/middlewares';
+
+// initial VALUES
+import { subState } from '@store/state';
+import { AuthState } from './types';

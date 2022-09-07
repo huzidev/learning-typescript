d30f0c23e00c4dd5d6dbcd26aea32aae6b426387
huzidev <huzaifa.iqdev@gmail.com>
2022-09-07 10:09:37 +0500

    axios service

---
+++ typescript-adonisJS/typescript-adonisJS-web/src/services/api.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import applyCaseMiddleware from 'axios-case-converter';
+
+const api = applyCaseMiddleware(
+  axios.create({
+    baseURL: 'http://127.0.0.1:3333/api',
+  }),
+  {
+    ignoreHeaders: true,
+    caseMiddleware: {
+      requestTransformer: (d) => d,
+      requestInterceptor: (config) => config,
+    },
+  },
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (!error?.response) {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      return Promise.reject({
+        response: {
+          data: {
+            data: null,
+            message: 'Cannot connect with the server',
+          },
+        },
+      });
+    }
+    return Promise.reject(error);
+  },
+);
+
+export function setToken(token: string | null | undefined): void {
+  const parsed = token ? `Bearer ${token}` : token;
+  api.defaults.headers.Authorization = parsed;
+}
+
+export default api;

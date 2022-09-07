5b53199f741ff26edeb054821a44c9c76553369a
huzidev <huzaifa.iqdev@gmail.com>
2022-09-07 09:56:58 +0500

    paths

--- typescript-adonisJS/typescript-adonisJS-web/src/store/auth/enpoints.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/store/auth/enpoints.ts
@@ -1,4 +1,4 @@
-// same paths defined in backend (AdonisJS)
+// same paths are defined in backend (AdonisJS)
 export const SIGN_IN = '/auth/v1/sign_in';
 export const SIGN_UP = '/auth/v1/sign_up';
 export const SIGN_OUT = '/auth/v1/sign_out';

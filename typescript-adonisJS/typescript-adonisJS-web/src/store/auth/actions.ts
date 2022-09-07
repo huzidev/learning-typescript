6588e38bd9720fe01841ca83322f12e79e1268d9
huzidev <huzaifa.iqdev@gmail.com>
2022-09-07 10:34:42 +0500

    local storage async

--- typescript-adonisJS/typescript-adonisJS-web/src/store/auth/actions.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/store/auth/actions.ts
@@ -1,3 +1,8 @@
 import { mapErrorToState } from '@store/utils';
 import { Action } from '@store/types';
 import { errorNotification } from 'utils/notifications';
+
+import api, { setToken } from '@services/api';
+
+// localStorage to save user's Token
+import storage from '@services/storage';

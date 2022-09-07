33657d61b3387a89aaa15789a57d04efdcb60b9f
huzidev <huzaifa.iqdev@gmail.com>
2022-09-07 09:45:53 +0500

    utils error fix

---
+++ typescript-adonisJS/typescript-adonisJS-web/src/store/utils.ts
@@ -0,0 +1,14 @@
+import { MapErrorToState } from "./types";
+
+// eslint-disable-next-line
+export function mapErrorToState(error: Record<string, any>): MapErrorToState {
+    const resp: MapErrorToState = error.response.data;
+    const state: MapErrorToState = {};
+    if (resp.message) {
+        state.message = resp.message
+    }
+    if (resp.errors) {
+        state.errors = resp.errors
+    }
+    return state;
+}

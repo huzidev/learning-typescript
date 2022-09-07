7568a1c1db205c1659e47bcec68985930fbcc4ef
huzidev <huzaifa.iqdev@gmail.com>
2022-09-07 09:52:23 +0500

    comments

--- project/happyhomes-web/src/store/state.ts
+++ project/happyhomes-web/src/store/state.ts
@@ -1,5 +1,6 @@
 import { SubState } from './types';
 
+// for INTERFACE we've to make first word Capital and (SubState) is an interface while (subState) with camel case is initialValues
 export const subState: SubState = {
   loading: false,
   error: false,

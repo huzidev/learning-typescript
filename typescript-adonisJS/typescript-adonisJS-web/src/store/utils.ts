dc117b3c0bc0edde6d39cfa26f05d3e4677d9862
huzidev <huzaifa.iqdev@gmail.com>
2022-09-07 09:49:13 +0500

    state types

--- typescript-adonisJS/typescript-adonisJS-web/src/store/utils.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/store/utils.ts
@@ -2,6 +2,8 @@ import { MapErrorToState } from "./types";
 
 // eslint-disable-next-line
 export function mapErrorToState(error: Record<string, any>): MapErrorToState {
+    // (string) for message and loading false, error true hence (any) type 
+
     const resp: MapErrorToState = error.response.data;
     const state: MapErrorToState = {};
     if (resp.message) {

6ebcdd4fd0a530543b04db491beacf4bb0c2b19e
huzidev <huzaifa.iqdev@gmail.com>
2022-09-10 11:06:29 +0500

    registeration form

--- typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Form/hooks.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Form/hooks.ts
@@ -12,6 +12,15 @@ function useHomeFormHook(): void {
     useEffect(() => {
         if (!prevAuth?.signInState?.error && auth.signInState.error) {
             errorNotification("Failed to logIn", auth.signInState)
+        } 
+        else if (prevAuth?.signInState.loading && !auth.signInState.loading) {
+            successNotification("LoggedIn", auth.signInState)
+        }
+        if (!prevAuth?.signUpState?.error && auth.signUpState.error) {
+            errorNotification("Failed to registered", auth.signUpState)
+        } 
+        else if (prevAuth?.signUpState.loading && !auth.signUpState.loading) {
+            successNotification("User registered", auth.signUpState)
         }
     }, [])
 }

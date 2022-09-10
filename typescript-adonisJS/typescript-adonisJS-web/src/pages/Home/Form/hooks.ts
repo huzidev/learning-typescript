84ea5a5c79e7eba99614480d6fe62e8c46703937
huzidev <huzaifa.iqdev@gmail.com>
2022-09-10 11:07:45 +0500

    hooks added

--- typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Form/hooks.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Form/hooks.ts
@@ -13,14 +13,14 @@ function useHomeFormHook(): void {
         if (!prevAuth?.signInState?.error && auth.signInState.error) {
             errorNotification("Failed to logIn", auth.signInState)
         } 
-        else if (prevAuth?.signInState.loading && !auth.signInState.loading) {
-            successNotification("LoggedIn", auth.signInState)
+        else if (prevAuth?.signInState?.loading && !auth.signInState.loading) {
+            successNotification("LoggedIn", auth.signInState.message)
         }
         if (!prevAuth?.signUpState?.error && auth.signUpState.error) {
             errorNotification("Failed to registered", auth.signUpState)
         } 
         else if (prevAuth?.signUpState.loading && !auth.signUpState.loading) {
-            successNotification("User registered", auth.signUpState)
+            successNotification("User registered", auth.signUpState.message)
         }
     }, [auth.signInState, auth.signUpState])
 }

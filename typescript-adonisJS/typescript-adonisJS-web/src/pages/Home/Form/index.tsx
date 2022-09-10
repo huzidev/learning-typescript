e339f90b31c309170c8ebf49b45da633feaa59e5
huzidev <huzaifa.iqdev@gmail.com>
2022-09-10 11:09:27 +0500

    cx alias class

--- typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Form/index.tsx
+++ typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Form/index.tsx
@@ -1,2 +1,4 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Button, Card, Checkbox, Col, Form, Input, Row, Typography } from 'antd';
+import { cx } from 'alias';

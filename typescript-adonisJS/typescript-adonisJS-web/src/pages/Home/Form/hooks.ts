60316954e5f717211ce0688b44b4a28e706f9faf
huzidev <huzaifa.iqdev@gmail.com>
2022-09-10 11:03:50 +0500

    useEffects

--- typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Form/hooks.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Form/hooks.ts
@@ -3,3 +3,15 @@ import { useEffect } from "react";
 import { useAuth } from "@store/auth";
 import { usePrevious } from "utils/hooks";
 import { errorNotification, successNotification } from 'utils/notifications';
+
+function useHomeFormHook(): void {
+    const auth = useAuth();
+    // usePrevious requires a parameter
+    const prevAuth = usePrevious(auth);
+
+    useEffect(() => {
+        if (!prevAuth?.signInState?.error && auth.signInState.error) {
+            errorNotification("Failed to logIn", auth.signInState)
+        }
+    }, [])
+}

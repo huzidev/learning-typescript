80db2d1f67a862782870f3dd51f5fcc4ce8feb40
huzidev <huzaifa.iqdev@gmail.com>
2022-09-12 09:56:05 +0500

    testing home-page

---
+++ typescript-adonisJS/typescript-adonisJS-web/src/components/Page/index.tsx
@@ -0,0 +1,29 @@
+import isObject from 'lodash/isObject';
+
+// import Header, { HeaderProps } from 'components/Header';
+// import Footer from 'components/Footer';
+
+export interface PageProps {
+//   header?: HeaderProps | boolean;
+  children: React.ReactNode;
+//   fullWidth?: boolean;
+//   footer?: boolean;
+}
+
+function Page({ children }: PageProps): JSX.Element {
+  return (
+    <>
+      {/* {header && <Header {...(isObject(header) ? header : {})} fullWidth={fullWidth} />} */}
+      {/* children is all the data at home page like between header and footer */}
+      {children}
+      {/* {footer && <Footer fullWidth={fullWidth} />} */}
+    </>
+  );
+}
+
+Page.defaultProps = {
+  fullWidth: false,
+  footer: false,
+};
+
+export default Page;

a51b4b41d48adf3c3fbba2ee2421d803af4b9c74
huzidev <huzaifa.iqdev@gmail.com>
2022-09-12 15:47:14 +0500

    custom data of array

--- typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Slider/data.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Slider/data.ts
@@ -2,16 +2,16 @@ export const slides = [
   {
     image: 'slide-1',
     title: 'Welcome',
-    description: 'Find your next dream home'
+    description: 'Find your next dream home',
   },
   {
     image: 'slide-2',
     title: 'Home',
-    description: 'Choose your next home'
+    description: 'Choose your next home',
   },
   {
     image: 'slide-3',
     title: 'Hello',
-    description: 'Welcome to happy homes'
+    description: 'Welcome to happy homes',
   },
 ];

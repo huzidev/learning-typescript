6ebb40fcb67403c345863e593f87188c2e5941b8
huzidev <huzaifa.iqdev@gmail.com>
2022-09-12 15:49:18 +0500

    useStrict js

--- typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Slider/index.tsx
+++ typescript-adonisJS/typescript-adonisJS-web/src/pages/Home/Slider/index.tsx
@@ -8,38 +8,37 @@ import { slides } from './data';
 import './styles.less';
 
 function HomeSlider(): JSX.Element {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const breaks = Grid.useBreakpoint();
+  const [activeIndex, setActiveIndex] = useState(0);
+  const breaks = Grid.useBreakpoint();
 
-    return(
-        <Carousel
-            fade
-            infinite
-            autoplay
-            autoplaySpeed={5000}
-            afterChange={(index) => setActiveIndex(index)}
+  return (
+    <Carousel
+      fade
+      infinite
+      autoplay
+      autoplaySpeed={5000}
+      afterChange={(index) => setActiveIndex(index)}
+    >
+      {slides.map((img, imgIndex) => (
+        <div className={cx('slide-base', img.image)} key={img.image}>
+          <div className={cx('slide-bg')} />
+          <Col
+            {...responsive.slider}
+            className={cx('slide-text', {
+              active: imgIndex === activeIndex,
+              mobile: !breaks.lg,
+            })}
         >
-            {
-                slides.map((img, imgIndex) => (
-                    <div className={cx('slide-base', img.image)} key={img.image}>
-                        <div className={cx('slide-bg')} />
-                        <Col
-                            {...responsive.slider}
-                            className={cx('slide-text', {
-                                active: imgIndex === activeIndex,
-                                mobile: !breaks.lg
-                            })}
-                        >
-                            <Typography.Title level={1} className={cx('title')}>
-                                {img.title}
-                            </Typography.Title>
-                            <Typography.Title level={4} type="secondary" className={cx('description')}>
-                                {img.description}
-                            </Typography.Title>
-                        </Col>
-                    </div>
-                ))
-            }
+                <Typography.Title level={1} className={cx('title')}>
+                    {img.title}
+                </Typography.Title>
+                <Typography.Title level={4} type="secondary" className={cx('description')}>
+                    {img.description}
+                </Typography.Title>
+            </Col>
+        </div>
+        ))
+    }
         </Carousel>
     )
   }

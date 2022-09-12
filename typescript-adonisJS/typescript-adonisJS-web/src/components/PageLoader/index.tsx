65c67f3fa1c255ef5c6fd7b3912335d5880a3f7c
huzidev <huzaifa.iqdev@gmail.com>
2022-09-12 13:27:03 +0500

    antd typography logo

--- typescript-adonisJS/typescript-adonisJS-web/src/components/PageLoader/index.tsx
+++ typescript-adonisJS/typescript-adonisJS-web/src/components/PageLoader/index.tsx
@@ -2,3 +2,28 @@ import { useEffect, useState } from 'react';
 import { Space, Spin } from 'antd';
 
 import { cx } from 'alias';
+
+import Logo from 'components/Logo';
+
+import './styles.less';
+
+function PageLoader(): JSX.Element {
+  const [state, setState] = useState(false);
+
+  useEffect(() => {
+    setState(true);
+  }, []);
+
+  return (
+    <div id="page-loader">
+      <div className={cx('content', { active: state })}>
+        <Space size="large">
+          <Logo level={1} />
+          <Spin size="large" />
+        </Space>
+      </div>
+    </div>
+  );
+}
+
+export default PageLoader;

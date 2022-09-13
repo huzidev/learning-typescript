697e3dab0b490d3eeeb95bc5acc0ac96fe18eeee
huzidev <huzaifa.iqdev@gmail.com>
2022-09-13 09:04:38 +0500

    types for user's data

--- typescript-adonisJS/typescript-adonisJS-web/src/store/utils.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/store/utils.ts
@@ -1,16 +1,16 @@
-import { MapErrorToState } from "./types";
+import { MapErrorToState } from './types';
 
 // eslint-disable-next-line
 export function mapErrorToState(error: Record<string, any>): MapErrorToState {
-    // (string) for message and loading false, error true hence (any) type 
+  // (string) for message and loading false, error true hence (any) type
 
-    const resp: MapErrorToState = error.response.data;
-    const state: MapErrorToState = {};
-    if (resp.message) {
-        state.message = resp.message
-    }
-    if (resp.errors) {
-        state.errors = resp.errors
-    }
-    return state;
+  const resp: MapErrorToState = error.response.data;
+  const state: MapErrorToState = {};
+  if (resp.message) {
+    state.message = resp.message
+  }
+  if (resp.errors) {
+    state.errors = resp.errors
+  }
+  return state;
 }

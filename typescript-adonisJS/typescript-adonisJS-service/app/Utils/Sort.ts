abbb30ecc499388dcd9061bafec4a54603a3f025
huzidev <huzaifa.iqdev@gmail.com>
2022-09-13 09:50:16 +0500

    filters utils

---
+++ typescript-adonisJS/typescript-adonisJS-service/app/Utils/Sort.ts
@@ -0,0 +1,20 @@
+import { LucidModel, LucidRow, ModelQueryBuilderContract } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Sort {
+  public static types = ['asc', 'desc'] as const
+
+  public static mapObjToQuery = (
+    obj: any,
+    query: ModelQueryBuilderContract<LucidModel, LucidRow>
+  ): void => {
+    console.log('object Type', obj)
+
+    if (!obj) {
+      return
+    }
+    Object.keys(obj).forEach((sortKey) => {
+      const sort = obj[sortKey]
+      query.orderBy(sortKey, sort)
+    })
+  }
+}

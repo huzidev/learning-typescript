78b8c354a54ca7fb30aac0f2304a9c52a60409d6
huzidev <huzaifa.iqdev@gmail.com>
2022-09-13 08:58:03 +0500

    fixing indentations

--- typescript-adonisJS/typescript-adonisJS-web/src/store/auth/index.ts
+++ typescript-adonisJS/typescript-adonisJS-web/src/store/auth/index.ts
@@ -9,15 +9,15 @@ import { AuthState } from './types';
 import * as actions from './actions';
 
 export const useAuth = create<AuthState>(
-    immerMiddleware((set) => ({
-        signInState: subState,
-        signUpState: subState,
-        signOutState: subState,
-        initState: { ...subState, init: false },
-        signIn: actions.signIn(set),
-        signUp: actions.signUp(set),
-        initUser: actions.initUser(set),
-        signOut: actions.signOut(set),
-        userVerified: actions.userVerified(set)
-    }))
+  immerMiddleware((set) => ({
+    signInState: subState,
+    signUpState: subState,
+    signOutState: subState,
+    initState: { ...subState, init: false },
+    signIn: actions.signIn(set),
+    signUp: actions.signUp(set),
+    initUser: actions.initUser(set),
+    signOut: actions.signOut(set),
+    userVerified: actions.userVerified(set),
+  })),
 );

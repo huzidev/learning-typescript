82917fa4b548d703f92a954040550dc8e07a46dc
huzidev <huzaifa.iqdev@gmail.com>
2022-09-13 09:59:28 +0500

    database seeders

--- typescript-adonisJS/typescript-adonisJS-service/database/seeders/MainSeeder/index.ts
+++ typescript-adonisJS/typescript-adonisJS-service/database/seeders/MainSeeder/index.ts
@@ -2,10 +2,6 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 
 export default class IndexSeeder extends BaseSeeder {
   private async runSeeder(seeder: { default: typeof BaseSeeder }) {
-    // if (seeder.default.developmentOnly && !Application.inDev) {
-    //   return
-    // }
-
     await new seeder.default(this.client).run()
   }
   public async run() {

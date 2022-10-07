cc17820d65583cc1f38fcb2a73822f4417d38e4c
huzidev <huzaifa.iqdev@gmail.com>
2022-10-07 09:30:45 +0500

    place holder

--- contact-form-firebase/src/components/contact-page/Contact.tsx
+++ contact-form-firebase/src/components/contact-page/Contact.tsx
@@ -4,7 +4,11 @@ export default function Contact() {
   return (
     <div>
         <form method='POST'>
-
+            <input 
+                type='text'
+                name='name'
+                placeholder='Enter Yours Name' 
+            />
         </form>
     </div>
   )

750ef52eeeab10be43d6d2777692829bf1dcd9c9
huzidev <huzaifa.iqdev@gmail.com>
2022-10-07 09:36:22 +0500

    input tag for data

--- contact-form-firebase/src/components/contact-page/Contact.tsx
+++ contact-form-firebase/src/components/contact-page/Contact.tsx
@@ -10,6 +10,30 @@ export default function Contact() {
                 placeholder='Enter Yours Name'
                 required
             />
+            <input 
+                type='email'
+                name='email'
+                placeholder='Enter Yours Email'
+                required
+            />
+            <input 
+                type='text'
+                name='name'
+                placeholder='Enter Yours Name'
+                required
+            />
+            <input 
+                type='text'
+                name='name'
+                placeholder='Enter Yours Name'
+                required
+            />
+            <input 
+                type='text'
+                name='name'
+                placeholder='Enter Yours Name'
+                required
+            />
         </form>
     </div>
   )

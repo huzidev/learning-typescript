33503df308b532cbd200ce5ebf5c5b3909870681
huzidev <huzaifa.iqdev@gmail.com>
2022-10-07 09:37:05 +0500

    componenets imported

--- contact-form-firebase/src/components/App.tsx
+++ contact-form-firebase/src/components/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Contact from './contact-page/Contact'
 
 export default function App() {
   return (
     <div>
       Hello, World!
+      <Contact />
     </div>
   )
 }

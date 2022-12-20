03f0490f398b006dfc8556f4931175fec8e6f53c
huzidev <huzaifa.iqdev@gmail.com>
2022-12-20 11:25:30 +0500

    dispatch redux

--- redux-thunk/src/components/App.tsx
+++ redux-thunk/src/components/App.tsx
@@ -1,7 +1,18 @@
+import { useDispatch } from "react-redux"
+
 export default function App(): JSX.Element {
+  const dispatch = useDispatch();
+  function onLoad() {
+  }
+
   return (
     <div>
-        Redux Thunk
+        <h1>
+            Redux Thunk
+        </h1>
+        <button>
+            Fetch Data
+        </button>
     </div>
   )
 }

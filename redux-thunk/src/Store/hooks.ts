f941b2a5b033fb2dd0a08ed928b2045c2c793456
huzidev <huzaifa.iqdev@gmail.com>
2022-12-21 08:43:33 +0500

    typed version

--- redux-thunk/src/Store/hooks.ts
+++ redux-thunk/src/Store/hooks.ts
@@ -1,6 +1,8 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "./index";
 
-// now export the useSelector and useDispatch with their Types
+// now export the useSelector and useDispatch with their TypedVersion
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch = () => useDispatch<AppDispatch>(); // because it'll runs/send function therefore using return fucntion
+
+// we'll use these export in every components

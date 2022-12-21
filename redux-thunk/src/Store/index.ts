8131a80eb3503e437f65555471bcf083950effb7
huzidev <huzaifa.iqdev@gmail.com>
2022-12-21 08:37:43 +0500

    export type added

--- redux-thunk/src/Store/index.ts
+++ redux-thunk/src/Store/index.ts
@@ -8,3 +8,7 @@ const store = configureStore({
 })
 
 export default store;
+
+// will use these two types for useSelector(for State) and for useDispatch(to run/send) function
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
